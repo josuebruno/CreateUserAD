4e79a91b2ca877762c51cb2993a48debf442a143
Josué Soares Bruno <73852814+josuebruno@users.noreply.github.com>
2022-07-27 14:37:23 -0300

    convertedata
    Converte data para padrão ad, recebe $date do php e trata conforme solicitado.

---
+++ sistema/webcriaruserad/convertdata.ps1
@@ -0,0 +1,18 @@
+﻿param(
+[string]$date
+)
+
+#$date = "2040-05-25"
+
+$arraydate = $date -split "-"
+
+$ano = $arraydate[0]
+$mes = $arraydate[1]
+$dia = $arraydate[2]
+
+$date = $ano+$mes+$dia
+
+$dateparsed = [datetime]::ParseExact($date,'yyyyMMdd',$null)
+$dateparsed = $dateparsed.ToFileTime()
+
+$dateparsed
